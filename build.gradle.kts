374ba39b2eef1e21f5d6ba9047c4ea4b0f9b0305
SIMULATAN <jakob@simulatan.me>
2025-01-17 15:21:24 +0100

    chore(deps): Koin-Annotations 2.0.0-Beta3, Koin 4.0.1, Kotlin 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
     kotlin("jvm") version "2.0.20"
-    id("com.google.devtools.ksp") version "2.0.20-1.0.24"
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25"
 }
 
 group = "me.simulatan.koin.getall"
-version = "1.0-SNAPSHOT"
+version = "2.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("io.insert-koin:koin-core:4.0.0-RC1")
-    implementation("io.insert-koin:koin-annotations:1.4.0-RC4")
-    ksp("io.insert-koin:koin-ksp-compiler:1.4.0-RC4")
+    implementation("io.insert-koin:koin-core:4.0.1")
+    implementation("io.insert-koin:koin-annotations:2.0.0-Beta3")
+    ksp("io.insert-koin:koin-ksp-compiler:2.0.0-Beta3")
 }
